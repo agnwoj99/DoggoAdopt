990e5d12a22b8db5fefb9cb22b11ce59e0cb572a
Agnieszka Wojtas <agnieszka@MacBook-Air-Agnieszka.local>
2021-01-13 20:00:42 +0100

    Add initial files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// skrypt naprawiający nawigację //
+$(document).click(function (event) {
+  var clickover = $(event.target);
+  var _opened = $(".navbar-collapse").hasClass("show");
+  if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+    $(".navbar-toggler").click();
+  }
+});
